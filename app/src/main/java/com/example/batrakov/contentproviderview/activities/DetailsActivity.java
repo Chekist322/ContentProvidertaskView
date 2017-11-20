726e66a0bbab62d4a452a43c7f97f8b22264f8b0
batrakov <phoenixed.451@gmail.com>
2017-11-20 17:06:10 +0300

    Update

--- app/src/main/java/com/example/batrakov/contentproviderview/activities/DetailsActivity.java
+++ app/src/main/java/com/example/batrakov/contentproviderview/activities/DetailsActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.batrakov.contentproviderview.R;
 
@@ -14,10 +15,30 @@ import com.example.batrakov.contentproviderview.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+
+    private TextView mType;
+    private TextView mName;
+    private TextView mAge;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_layout);
+
+        mType = findViewById(R.id.type);
+        mName = findViewById(R.id.name);
+        mAge = findViewById(R.id.age);
+
+        int clickedElementPosition = (int) getIntent().getLongExtra(MainListActivity.CLICKED_ITEM_ID, -1);
+
+        if (clickedElementPosition != -1) {
+            Uri uri = getIntent().getParcelableExtra(MainListActivity.CHOSEN_TYPE_URI);
+            uri = uri.buildUpon().appendPath(String.valueOf(clickedElementPosition)).build();
+            Cursor cursor = getContentResolver().query(uri, null, null,
+                    null, null);
+            cursor.moveToFirst();
+            System.out.println(cursor.getString(1));
+        }
     }
 
 }
--- app/src/main/java/com/example/batrakov/contentproviderview/activities/MainListActivity.java
+++ app/src/main/java/com/example/batrakov/contentproviderview/activities/MainListActivity.java
@@ -3,7 +3,6 @@ package com.example.batrakov.contentproviderview.activities;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,11 +12,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.batrakov.contentproviderview.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainListActivity extends AppCompatActivity {
+
+    public static final String CLICKED_ITEM_ID = "item id";
+    public static final String CHOSEN_TYPE_URI = "type";
 
     private static final Uri FOXES_URI = Uri
             .parse("content://com.example.batrakov.contentproviderdatabase.authority/foxes");
@@ -25,12 +31,15 @@ public class MainListActivity extends AppCompatActivity {
     private static final Uri BADGERS_URI = Uri
             .parse("content://com.example.batrakov.contentproviderdatabase.authority/badgers");
 
-    private static final String CLICKED_ITEM_ID = "item id";
-    private static final int CLICKED_ITEM_URI = 1;
+    private static final String sFromTableFieldsFrom[] = { "_id", "name" };
+    private static final int sTargetListViews[] = { android.R.id.text1, android.R.id.text2 };
+    private Uri mCurrentListContentUri;
 
-    Button mFoxesButton;
-    Button mBadgersButton;
-    ListView mListView;
+    private Button mFoxesButton;
+    private Button mBadgersButton;
+    private TextView mTimeStamp;
+    private ListView mListView;
+    private SimpleCursorAdapter mCursorAdapter;
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
@@ -41,40 +50,48 @@ public class MainListActivity extends AppCompatActivity {
         mFoxesButton = findViewById(R.id.foxes);
         mBadgersButton = findViewById(R.id.badgers);
         mListView = findViewById(R.id.list);
+        mTimeStamp = findViewById(R.id.timeStamp);
 
         mFoxesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Cursor cursor = getContentResolver().query(FOXES_URI, null, null,
-                        null, null);
-                String from[] = { "_id", "name" };
-                int to[] = { android.R.id.text1, android.R.id.text2 };
-                SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.simple_list_item_2, cursor, from, to, 0);
-
-                mListView.setAdapter(adapter);
+                loadListFromDB(FOXES_URI);
             }
         });
 
         mBadgersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Cursor cursor = getContentResolver().query(BADGERS_URI, null, null,
-                        null, null);
-                String from[] = { "_id", "name" };
-                int to[] = { android.R.id.text1, android.R.id.text2 };
-                SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.simple_list_item_2, cursor, from, to, 0);
-
-                mListView.setAdapter(adapter);
+                loadListFromDB(BADGERS_URI);
             }
         });
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "id " + String.valueOf(id), Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getBaseContext(), DetailsActivity.class);
-                intent.putExtra(CLICKED_ITEM_ID, id);
+                Toast.makeText(getApplicationContext(), "position " + String.valueOf(position), Toast.LENGTH_SHORT).show();
+
+                Intent startDetailActivityIntent = new Intent(getBaseContext(), DetailsActivity.class);
+                startDetailActivityIntent.putExtra(CLICKED_ITEM_ID, id);
+                startDetailActivityIntent.putExtra(CHOSEN_TYPE_URI, mCurrentListContentUri);
+                startActivity(startDetailActivityIntent);
             }
         });
     }
+
+    private void loadListFromDB(Uri aUri) {
+        Cursor cursor = getContentResolver().query(aUri, null, null,
+                null, null);
+        mCursorAdapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.simple_list_item_2,
+                cursor, sFromTableFieldsFrom, sTargetListViews, 0);
+        mListView.setAdapter(mCursorAdapter);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String timeStamp = currentTime.toString();
+        mTimeStamp.setText(timeStamp);
+
+        mCurrentListContentUri = aUri;
+    }
+
+
 }
