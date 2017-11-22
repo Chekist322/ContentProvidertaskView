0507df5926733f9739788ccad72893f60e0aadb6
batrakov <phoenixed.451@gmail.com>
2017-11-22 11:31:55 +0300

    Change buttons to tabs.

--- app/src/main/java/com/example/batrakov/contentproviderview/activities/MainListActivity.java
+++ app/src/main/java/com/example/batrakov/contentproviderview/activities/MainListActivity.java
@@ -15,10 +15,9 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.batrakov.contentproviderview.R;
 
@@ -51,10 +50,10 @@ public class MainListActivity extends AppCompatActivity {
     private static final String[] TARGET_TABLE_COLUMNS_NAME = {"_id", "name"};
     private static final int[] TARGET_LIST_ITEM_VIEWS = {android.R.id.text1, android.R.id.text2};
     private static final String SAVED_TIME_STAMP = "time stamp";
-    private Uri mCurrentListContentUri = Uri.EMPTY;
+    private static final int FOXES_LOADER_ID = 0;
+    private static final int BADGERS_LOADER_ID = 1;
 
     private TextView mTimeStamp;
-    private ListView mListView;
     private SimpleCursorAdapter mFoxesCursorAdapter;
     private SimpleCursorAdapter mBadgersCursorAdapter;
 
@@ -64,6 +63,30 @@ public class MainListActivity extends AppCompatActivity {
         super.onCreate(aSavedInstanceState);
         setContentView(R.layout.main_list_layout);
 
+        mTimeStamp = findViewById(R.id.timeStamp);
+        ListView foxesListView = findViewById(R.id.list_foxes);
+        ListView badgersListView = findViewById(R.id.list_badgers);
+
+        TabHost tabHost = findViewById(R.id.tab_host);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec("foxes");
+        tabSpec.setIndicator(getString(R.string.foxes));
+        tabSpec.setContent(R.id.list_foxes);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("badgers");
+        tabSpec.setIndicator(getString(R.string.badgers));
+        tabSpec.setContent(R.id.list_badgers);
+        tabHost.addTab(tabSpec);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.contains(SAVED_TIME_STAMP)) {
+            mTimeStamp.setText(preferences.getString(SAVED_TIME_STAMP, ""));
+        }
+
         mFoxesCursorAdapter = new SimpleCursorAdapter(getApplicationContext(),
                 android.R.layout.simple_list_item_2,
                 null, TARGET_TABLE_COLUMNS_NAME, TARGET_LIST_ITEM_VIEWS, 0);
@@ -72,52 +95,31 @@ public class MainListActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_2,
                 null, TARGET_TABLE_COLUMNS_NAME, TARGET_LIST_ITEM_VIEWS, 0);
 
-        Button foxesButton = findViewById(R.id.foxes);
-        Button badgersButton = findViewById(R.id.badgers);
-        mListView = findViewById(R.id.list);
-        mTimeStamp = findViewById(R.id.timeStamp);
+        foxesListView.setAdapter(mFoxesCursorAdapter);
+        badgersListView.setAdapter(mBadgersCursorAdapter);
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (preferences.contains(SAVED_TIME_STAMP)) {
-            mTimeStamp.setText(preferences.getString(SAVED_TIME_STAMP, ""));
-        }
-
-        foxesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View aView) {
-                try {
-                    loadListFromDB(FOXES_URI);
-                } catch (SecurityException aE) {
-                    Toast.makeText(getApplicationContext(),
-                            "Sorry, you have no permission for this :(", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
-
-        badgersButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View aView) {
-                try {
-                    loadListFromDB(BADGERS_URI);
-                } catch (SecurityException aE) {
-                    Toast.makeText(getApplicationContext(),
-                            "Sorry, you have no permission for this :(", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        foxesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> aParent, View aView, int aPosition, long aId) {
                 Intent startDetailActivityIntent = new Intent(getBaseContext(), DetailsActivity.class);
                 startDetailActivityIntent.putExtra(CLICKED_ITEM_ID, aId);
-                startDetailActivityIntent.putExtra(CHOSEN_TYPE_URI, mCurrentListContentUri);
+                startDetailActivityIntent.putExtra(CHOSEN_TYPE_URI, FOXES_URI);
                 startActivity(startDetailActivityIntent);
             }
         });
 
-        getLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+        badgersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> aParent, View aView, int aPosition, long aId) {
+                Intent startDetailActivityIntent = new Intent(getBaseContext(), DetailsActivity.class);
+                startDetailActivityIntent.putExtra(CLICKED_ITEM_ID, aId);
+                startDetailActivityIntent.putExtra(CHOSEN_TYPE_URI, BADGERS_URI);
+                startActivity(startDetailActivityIntent);
+            }
+        });
+
+
+        getLoaderManager().initLoader(FOXES_LOADER_ID, null, new LoaderManager.LoaderCallbacks<Cursor>() {
             @Override
             public Loader<Cursor> onCreateLoader(int aId, Bundle aArgs) {
                 return new CursorLoader(getApplicationContext(), FOXES_URI, null, null, null, null);
@@ -126,6 +128,12 @@ public class MainListActivity extends AppCompatActivity {
             @Override
             public void onLoadFinished(Loader<Cursor> aLoader, Cursor aCursor) {
                 mFoxesCursorAdapter.swapCursor(aCursor);
+
+                Date currentTime = Calendar.getInstance().getTime();
+                String timeStamp = currentTime.toString();
+                mTimeStamp.setText(timeStamp);
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit()
+                        .putString(SAVED_TIME_STAMP, mTimeStamp.getText().toString()).apply();
             }
 
             @Override
@@ -133,7 +141,7 @@ public class MainListActivity extends AppCompatActivity {
             }
         });
 
-        getLoaderManager().initLoader(1, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+        getLoaderManager().initLoader(BADGERS_LOADER_ID, null, new LoaderManager.LoaderCallbacks<Cursor>() {
             @Override
             public Loader<Cursor> onCreateLoader(int aId, Bundle aArgs) {
                 return new CursorLoader(getApplicationContext(), BADGERS_URI, null, null, null, null);
@@ -142,6 +150,12 @@ public class MainListActivity extends AppCompatActivity {
             @Override
             public void onLoadFinished(Loader<Cursor> aLoader, Cursor aData) {
                 mBadgersCursorAdapter.swapCursor(aData);
+
+                Date currentTime = Calendar.getInstance().getTime();
+                String timeStamp = currentTime.toString();
+                mTimeStamp.setText(timeStamp);
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit()
+                        .putString(SAVED_TIME_STAMP, mTimeStamp.getText().toString()).apply();
             }
 
             @Override
@@ -151,28 +165,28 @@ public class MainListActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Send request to Database and fill ListView by incoming information.
-     *
-     * @param aUri target Uri query.
-     *
-     * @throws SecurityException on content provider permission deny.
-     */
-    private void loadListFromDB(Uri aUri) throws SecurityException {
-
-        Date currentTime = Calendar.getInstance().getTime();
-        String timeStamp = currentTime.toString();
-        mTimeStamp.setText(timeStamp);
-
-        PreferenceManager.getDefaultSharedPreferences(this).edit()
-                .putString(SAVED_TIME_STAMP, mTimeStamp.getText().toString()).apply();
-
-        mCurrentListContentUri = aUri;
-
-        if (aUri.equals(FOXES_URI)) {
-            mListView.setAdapter(mFoxesCursorAdapter);
-        } else if (aUri.equals(BADGERS_URI)) {
-            mListView.setAdapter(mBadgersCursorAdapter);
-        }
-    }
+//    /**
+//     * Send request to Database and fill ListView by incoming information.
+//     *
+//     * @param aUri target Uri query.
+//     *
+//     * @throws SecurityException on content provider permission deny.
+//     */
+//    private void loadListFromDB(Uri aUri) throws SecurityException {
+//
+//        Date currentTime = Calendar.getInstance().getTime();
+//        String timeStamp = currentTime.toString();
+//        mTimeStamp.setText(timeStamp);
+//
+//        PreferenceManager.getDefaultSharedPreferences(this).edit()
+//                .putString(SAVED_TIME_STAMP, mTimeStamp.getText().toString()).apply();
+//
+//        mCurrentListContentUri = aUri;
+//
+//        if (aUri.equals(FOXES_URI)) {
+//            mListView.setAdapter(mFoxesCursorAdapter);
+//        } else if (aUri.equals(BADGERS_URI)) {
+//            mListView.setAdapter(mBadgersCursorAdapter);
+//        }
+//    }
 }
